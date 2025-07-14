63a16b6d84579ccfa70df846038faa901f1e8ced
Ama-dej <amadej.arh@gmail.com>
2025-07-14 21:35:08 +0200

    \n

--- build/CMakeFiles/emzasmlib.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libemzasmlib.a"
-  "libemzasmlib.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/emzasmlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
